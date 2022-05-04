96ce5ad7224d63dbcc77b134c9b7c99290b8ce7c
rociobessio <rocibessio@gmail.com>
2022-05-04 13:45:29 -0300

    tp 2 subida

---
+++ TP_[2]/src/menuPrincipal.h
@@ -0,0 +1,18 @@
+/*
+ * menuPrincipal.h
+ *
+ *  Created on: 19 abr. 2022
+ *      Author: claudia
+ */
+
+#ifndef MENUPRINCIPAL_H_
+#define MENUPRINCIPAL_H_
+
+/**
+ * brief Lleva al usuario al menu principal y para que de comienzo al programa
+ */
+void menuPrincipal();
+
+
+
+#endif /* MENUPRINCIPAL_H_ */
